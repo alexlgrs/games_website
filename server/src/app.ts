42bc2bed357235bf649b74aea9a728810fc8677a
alexlgrs <alex.lgrs22@gmail.com>
2026-01-20 10:41:43 +0100

    client ajouté et lien entre les 2 fait, manque tailwind

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 import userRoutes from "./routes/UserRoutes";
 
 dotenv.config();
@@ -10,6 +11,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(
+  express.static(path.join(__dirname, "..", "..", "client", "build"))
+);
+
 app.use("/api/users", userRoutes);
+
+app.get("/{*any}", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "..", "..", "client", "build", "index.html")
+  );
+});
 
 export default app;
